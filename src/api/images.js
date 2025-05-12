9f22bd52d28df805bda784e15b1f32076c41ee1c
Oleksandra Ovcharova <oleksaovcharova@gmail.com>
2025-05-12 23:32:54 +0200

    fixed per page

--- src/api/images.js
+++ src/api/images.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const fetchImagesWithFilters = async (search, page) => {
   const response = await axios.get(
-    `/search/photos?query=${search}&page=${page}`,
+    `/search/photos?query=${search}&page=${page}&per_page=12`,
     {
       headers: {
         Authorization: "Client-ID e_McKAw9UynSdA6MKajMp71Ldn2cdgCe70RGTPNYxR4",
